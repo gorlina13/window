5b7181745e42e4703265048d06cc1abd83c58ecf
Olga Bazhenova <olgabazhenova07@yandex.ru>
2020-05-09 22:20:39 +0300

    Дополняет main.js

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,13 @@
+import 'element-closest-polyfill';
 import './slider.js';
+import modal from './modules/modal';
+
+function work() {
+  modal();
+}
+
+if (document.readyState === `loading`) {
+  document.addEventListener(`DOMContentLoaded`, work);
+} else {
+  work();
+}
