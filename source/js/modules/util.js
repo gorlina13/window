4c8d884f36a8d53f838e734b2546f9d375883fff
Olga Bazhenova <olgabazhenova07@yandex.ru>
2020-05-20 02:04:24 +0300

    Дополняет util.js для поддержки IE11

--- source/js/modules/util.js
+++ source/js/modules/util.js
@@ -30,21 +30,21 @@ function checkArrowRight(evt) {
   const ARROW_RIGHT_KEYCODE = 39;
   const key = evt.key || evt.keyCode;
 
-  return key === `ArrowRight` || key === ARROW_RIGHT_KEYCODE;
+  return key === `ArrowRight` || key === `Right` || key === ARROW_RIGHT_KEYCODE;
 }
 
 function checkArrowLeft(evt) {
   const ARROW_LEFT_KEYCODE = 37;
   const key = evt.key || evt.keyCode;
 
-  return key === `ArrowLeft` || key === ARROW_LEFT_KEYCODE;
+  return key === `ArrowLeft` || key === `Left` || key === ARROW_LEFT_KEYCODE;
 }
 
 function checkArrowDown(evt) {
   const ARROW_DOWN_KEYCODE = 40;
   const key = evt.key || evt.keyCode;
 
-  return key === `ArrowDown ` || key === ARROW_DOWN_KEYCODE;
+  return key === `ArrowDown` || key === `Down` || key === ARROW_DOWN_KEYCODE;
 }
 
 export {
